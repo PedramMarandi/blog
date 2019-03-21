1b48a99c3feae3f10249c6cef4e00f0331cb1df3
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2019-03-21 17:51:33 +0330

    move theme inside styles/themes

--- src/components/BlogLayout/index.js
+++ src/components/BlogLayout/index.js
@@ -1,15 +1,14 @@
 import React, { Fragment } from "react"
 import { ThemeProvider } from "styled-components"
-import { GlobalStyle } from "./globalStyle"
-
-const theme = {
-  main: "mediumseagreen",
-}
+import { GlobalStyle } from "../../style/globalStyle"
+import { Header } from "../Header"
+import { theme } from "../../style/themes"
 
 export const BlogLayout = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
       <Fragment>
+        <Header />
         <div className="children">{children}</div>
         <GlobalStyle />
       </Fragment>
---
+++ src/style/themes.js
@@ -0,0 +1,5 @@
+export const theme = {
+  backColor: "#fff",
+  primary: "#42A5F5",
+  textColor: "#000"
+}
