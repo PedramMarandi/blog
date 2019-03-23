ed361e726e1732627a3edcf467a9362e28ed3b99
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2019-03-23 19:49:15 +0430

    render posts by postLists component

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -3,19 +3,35 @@ import PropTypes from "prop-types"
 import styled from "styled-components"
 import { Link } from "gatsby"
 import { H3 } from "../H3"
+import { PostHtml } from "../PostHtml"
+import Wave from "./wave.svg"
 
 const Div = styled("div")`
-  a {
+  .postList__title {
     text-decoration: none;
     color: ${props => props.theme.textColor};
   }
   h3:hover {
-    a {
+    .postList__title {
       color: ${props => props.theme.titleHoverColor};
     }
   }
   .postList__p {
     margin-top: var(--base-line);
+  }
+  &:after {
+    width: 125px;
+    height: 25px;
+    display: block;
+    content: "";
+    background-image: url(${Wave});
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    margin: var(--base-line) auto;
+  }
+  &:last-of-type:after {
+    display: none;
   }
 `
 export const PostList = ({ frontmatter, excerpt }) => {
@@ -23,9 +39,14 @@ export const PostList = ({ frontmatter, excerpt }) => {
   return (
     <Div>
       <H3>
-        <Link to={path}>{title}</Link>
+        <Link className="postList__title" to={path}>
+          {title}
+        </Link>
       </H3>
-      <div className="postList__p" dangerouslySetInnerHTML={{ __html: excerpt }} />
+      <PostHtml
+        className="postList__p"
+        dangerouslySetInnerHTML={{ __html: excerpt }}
+      />
     </Div>
   )
 }
