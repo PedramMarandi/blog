73a8b4d73f06abaa9b5142af5a454d496b24d2d5
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2019-03-24 03:11:37 +0430

    improve H1 style

--- src/components/H1/index.js
+++ src/components/H1/index.js
@@ -3,9 +3,10 @@ import { mediumUp } from "../../style/breakpoints"
 
 export const H1 = styled('h1')`
   font-size: 1.618rem;
-  line-height: calc(3 * var(--base-line));
+  line-height: calc(2 * var(--base-line));
   color: ${props => props.theme.textColor};
   @media ${mediumUp} {
     font-size: 2.618rem;
+    line-height: calc(3 * var(--base-line));
   }
 `;
