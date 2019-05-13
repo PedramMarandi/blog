806927dd6ceed35ac71653bd5819ce26d862b20c
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2019-05-14 00:09:04 +0430

    add Aboutme texts

--- src/templates/about.js
+++ src/templates/about.js
@@ -1,16 +1,35 @@
 import React from "react"
 import { graphql } from "gatsby"
+import styled from "styled-components"
 import { BlogLayout } from "../components/BlogLayout"
+import { mediumUp } from "../style/breakpoints"
+import { PostHtml } from '../components/PostHtml';
 
+const AboutContainer = styled(PostHtml)`
+  margin-top: calc(3 * var(--base-line));
+  .post__content {
+    margin-top: calc(1 * var(--base-line));
+  }
+
+  h1 {
+    font-size: 1.618rem;
+    line-height: calc(2 * var(--base-line));
+    color: ${props => props.theme.textColor};
+    @media ${mediumUp} {
+      font-size: 2.618rem;
+      line-height: calc(3 * var(--base-line));
+    }
+  }
+`
 const AboutTemplate = ({ data }) => {
   const { markdownRemark } = data
   const { title } = markdownRemark.frontmatter
   const { html } = markdownRemark
   return (
     <BlogLayout>
-      <h2>About</h2>
-      <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <AboutContainer>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </AboutContainer>
     </BlogLayout>
   )
 }
